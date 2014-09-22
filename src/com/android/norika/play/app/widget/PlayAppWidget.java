eb70de444d60e2671d55575f0146252aaeca2832
Norika <jhbnorika@gmail.com>
2014-09-23 02:59:05 +0800

    widget change

--- src/com/android/norika/play/app/widget/PlayAppWidget.java
+++ src/com/android/norika/play/app/widget/PlayAppWidget.java
@@ -14,7 +14,17 @@ public class PlayAppWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.view_widget_timeslice);
+
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            int appWidgetId = appWidgetIds[i];
+            RemoteViews views = new RemoteViews(context.getPackageName(),
+                    R.layout.view_widget_timeslice);
+            AppWidgetManager appManager = AppWidgetManager.getInstance(context);
+            // ComponentName thisWidget = new ComponentName(context,
+            // PlayAppWidget.class);
+            appManager.updateAppWidget(appWidgetId, views);
+        }
     }
 
 }
