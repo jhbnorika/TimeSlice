99bce0e0b91b87d092b4341abb09c3c2561db073
Norika <hongbingji@anjuke.com>
2014-10-08 19:39:39 +0800

    base change

--- src/com/android/norika/play/app/LovePlayApp.java
+++ src/com/android/norika/play/app/LovePlayApp.java
@@ -1,27 +1,24 @@
 package com.android.norika.play.app;
 
-import com.android.norika.play.model.ColorType;
-
 import android.app.Application;
 
-public class OneSecondApp extends Application {
+import com.android.norika.play.model.ColorType;
+
+public class LovePlayApp extends Application {
 
 	@Override
 	public void onCreate() {
-
-		ColorType.init(this);
 		super.onCreate();
+        ColorType.init(this);
 	}
 
 	@Override
 	public void onLowMemory() {
-		// TODO Auto-generated method stub
 		super.onLowMemory();
 	}
 
 	@Override
 	public void onTerminate() {
-		// TODO Auto-generated method stub
 		super.onTerminate();
 	}
 
