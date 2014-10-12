1964c60f2d3788d8330e8f4fd0a5df5b96c0a6b3
Norika <jhbnorika@gmail.com>
2014-10-13 03:22:40 +0800

    Lib added some config changed

---
+++ src/com/android/norika/play/background/MessageService.java
@@ -0,0 +1,17 @@
+package com.android.norika.play.background;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class MessageService extends IntentService {
+
+    public MessageService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+}
---
+++ src/com/android/norika/play/ui/JustForFunActivity.java
@@ -0,0 +1,7 @@
+package com.android.norika.play.ui;
+
+import android.app.Activity;
+
+public class JustForFunActivity extends Activity {
+
+}
